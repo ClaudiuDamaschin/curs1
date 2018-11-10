4bd8dd662d34c7a8c906b1fbdd71371ed7293225
clau <clau@192.168.0.31>
2018-11-10 16:26:06 +0200

    third commit

---
+++ src/Exercitiul3.java
@@ -0,0 +1,14 @@
+//Sa se implementeze o metoda care returneaza media aritmetica a numerelor dintr-un sir de numere primit ca parametru.
+
+public class Exercitiul3 {
+	public static void main(String[] args) {
+		int firstNumber = 10;
+		int secondNumber = 5;
+		int mediaAritmetica;
+		if (firstNumber <= 0) {
+
+		}
+		mediaAritmetica = (firstNumber + secondNumber) / 2;
+		System.out.println("Media aritmetica este " + mediaAritmetica);
+	}
+}
