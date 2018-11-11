7f2d265730336fe8759c5efd36f22e92dd3e7c59
clau <clau@192.168.0.31>
2018-11-11 15:30:26 +0200

    third1 commit

--- src/Exercitiul3.java
+++ src/Exercitiul3.java
@@ -4,23 +4,22 @@ public class Exercitiul3 {
 
 	public static void main(String[] args) {
 
-		int[] array = { 10, 26, 38, 42, 51 };
+		int[] array = { 12, 28, 32, 9, 55 };
 
 		System.out.println(mediaAritmetica(array));
 
-		int suma = mediaAritmetica(array);
-
-		int media = suma / array.length;
-
 	}
 
-	// tu faci aici suma elementelor. 
-	// Inainte sa returnezi suma ar trebui s-o imparti la numarul de elemente ca sa-ti returneze media si ai rezolvat problema
+	// tu faci aici suma elementelor.
+	// Inainte sa returnezi suma ar trebui s-o imparti la numarul de elemente ca
+	// sa-ti returneze media si ai rezolvat problema
 	private static int mediaAritmetica(int[] array) {
 		int suma = 0;
 		for (int i = 0; i < array.length; i++) {
 			suma = suma + array[i];
+
 		}
+		int media = suma / array.length;
 		return media;
 	}
 }
