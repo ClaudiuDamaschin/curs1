6c07d49e5d3ac9b46b59ca38f0f0bd9dfaaeee4d
clau <clau@192.168.0.31>
2018-11-11 15:56:26 +0200

    fifth commit

---
+++ src/Exercitiul4.java
@@ -0,0 +1,22 @@
+
+//Sa se implementeze o metoda care returneaza produsul numerelor nenule dintr-un sir de numere primit ca parametru.
+
+public class Exercitiul4 {
+	public static void main(String[] args) {
+		int array[] = { 2, 5, 6, 7, 9, 8 };
+		System.out.println(productNumber(array));
+		productNumber(array);
+	}
+
+	private static int productNumber(int[] array) {
+		int product = 1;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == 0) {
+				continue;
+			}
+			product *= array[i];
+		}
+		return product;
+	}
+
+}
