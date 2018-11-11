491b48e3be77bbc792baca69a3da38b56b6eb922
clau <clau@192.168.0.31>
2018-11-11 16:05:41 +0200

    fifth1 commit

---
+++ src/Exercitiul5.java
@@ -0,0 +1,22 @@
+//Sa se implementeze o metoda care returneaza cate numere negative se gasesc intr-un sir de numere primit ca parametru.
+
+public class Exercitiul5 {
+	public static void main(String[] args) {
+		int array[] = { 7, -5, -5, -7, 2 };
+		System.out.println(numereNegative(array));
+		int negative = numereNegative(array);
+
+	}
+
+	private static int numereNegative(int[] array) {
+		int negative = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] < 0) {
+				negative++;
+			}
+
+		}
+		return negative;
+	}
+
+}
