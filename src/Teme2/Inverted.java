036aadd9b765da2f28855de022bc24eca2ba134a
clau <clau@192.168.0.31>
2018-11-17 21:06:56 +0200

    fifth1 commit

---
+++ src/Teme2/Inverted.java
@@ -0,0 +1,19 @@
+//6. Sa se implementeze o metoda care primeste ca parametru un sir de numere si returneaza sirul respectiv inversat
+
+package Teme2;
+
+public class Inverted {
+	public static void main(String[] args) {
+
+		int[] a = { 5, 8, 1, 2 };
+		inverted(a);
+	}
+
+	private static void inverted(int[] a) {
+		for (int i = a.length - 1; i >= 0; i--) {
+			System.out.print(a[i]);
+		}
+
+	}
+
+}
